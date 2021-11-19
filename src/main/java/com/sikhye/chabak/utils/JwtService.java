0e7d7f2571a358fdef6fc797c4e481a4bb1057c3
hjun-park <phj0860@gmail.com>
2021-11-19 23:07:01 +0900

    [2021-11-19]
    1. github action 위한 설정 파일 업로드(gpg)

--- src/main/java/com/sikhye/chabak/utils/JwtService.java
+++ src/main/java/com/sikhye/chabak/utils/JwtService.java
@@ -1,11 +1,11 @@
 package com.sikhye.chabak.utils;
 
 import com.sikhye.chabak.base.BaseException;
-import com.sikhye.chabak.config.secret.Secret;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -19,19 +19,26 @@ import static com.sikhye.chabak.base.BaseResponseStatus.INVALID_JWT;
 @Service
 public class JwtService {
 
+	@Value("${secret.JWT_SECRET_KEY}")
+	private String JWT_SECRET_KEY;
+
+	@Value("${secret.USER_INFO_PASSWORD_KEY}")
+	private String USER_INFO_PASSWORD_KEY;
+
+
 	/*
 	JWT 생성
 	@param userIdx
 	@return String
 	 */
-	public String createJwt(Long userIdx){
+	public String createJwt(Long userIdx) {
 		Date now = new Date();
 		return Jwts.builder()
-			.setHeaderParam("type","jwt")
-			.claim("userIdx",userIdx)
+			.setHeaderParam("type", "jwt")
+			.claim("userIdx", userIdx)
 			.setIssuedAt(now)
-			.setExpiration(new Date(System.currentTimeMillis()+1*(1000*60*60*24*365)))
-			.signWith(SignatureAlgorithm.HS256, Secret.JWT_SECRET_KEY)
+			.setExpiration(new Date(System.currentTimeMillis() + 1 * (1000 * 60 * 60 * 24 * 365)))
+			.signWith(SignatureAlgorithm.HS256, JWT_SECRET_KEY)
 			.compact();
 	}
 
@@ -39,7 +46,7 @@ public class JwtService {
 	Header에서 X-ACCESS-TOKEN 으로 JWT 추출
 	@return String
 	 */
-	public String getJwt(){
+	public String getJwt() {
 		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
 		return request.getHeader("X-ACCESS-TOKEN");
 	}
@@ -52,15 +59,15 @@ public class JwtService {
 	public Long getUserIdx() throws BaseException {
 		//1. JWT 추출
 		String accessToken = getJwt();
-		if(accessToken == null || accessToken.length() == 0){
+		if (accessToken == null || accessToken.length() == 0) {
 			throw new BaseException(EMPTY_JWT);
 		}
 
 		// 2. JWT parsing
 		Jws<Claims> claims;
-		try{
+		try {
 			claims = Jwts.parser()
-				.setSigningKey(Secret.JWT_SECRET_KEY)
+				.setSigningKey(JWT_SECRET_KEY)
 				.parseClaimsJws(accessToken);
 		} catch (Exception ignored) {
 			throw new BaseException(INVALID_JWT);
@@ -72,17 +79,17 @@ public class JwtService {
 		return Long.valueOf(String.valueOf(claims.getBody().get("userIdx")));
 	}
 
-	public Long getUserEmail(String token) throws BaseException{
+	public Long getUserEmail(String token) throws BaseException {
 		//1. JWT 추출
-		if(token == null || token.length() == 0){
+		if (token == null || token.length() == 0) {
 			throw new BaseException(EMPTY_JWT);
 		}
 
 		// 2. JWT parsing
 		Jws<Claims> claims;
-		try{
+		try {
 			claims = Jwts.parser()
-				.setSigningKey(Secret.JWT_SECRET_KEY)
+				.setSigningKey(JWT_SECRET_KEY)
 				.parseClaimsJws(token);
 		} catch (Exception ignored) {
 			throw new BaseException(INVALID_JWT);

4832298a54a1f08b9e84be8aa7bb507a3aaa1b9c
hjun-park <phj0860@gmail.com>
2021-11-24 00:50:28 +0900

    fix: 존재하지 않는 entity 대상으로 Q객체 생성하며 발생하는 문제 해결

--- src/main/java/com/sikhye/chabak/src/member/entity/Member.java
+++ src/main/java/com/sikhye/chabak/src/member/entity/Member.java
@@ -1,8 +1,6 @@
 package com.sikhye.chabak.src.member.entity;
 
 import com.sikhye.chabak.base.entity.BaseStatus;
-import com.sikhye.chabak.src.place.entity.PlaceImage;
-import com.sikhye.chabak.src.place.entity.PlaceReview;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,8 +8,6 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicInsert;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)

56ecc2128c565858c37ffc107e108189deb91bcd
hjun-park <phj0860@gmail.com>
2021-11-23 18:47:09 +0900

    Add: S3 업로드 시 폴더 이름도 함께 집어넣을 수 있도록 처리

--- src/main/java/com/sikhye/chabak/utils/aws/BasicUploadService.java
+++ src/main/java/com/sikhye/chabak/utils/aws/BasicUploadService.java
@@ -5,7 +5,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 public interface BasicUploadService {
-	String uploadImage(MultipartFile file);
+	String uploadImage(MultipartFile file, String folderName);
 
-	List<String> uploadImages(List<MultipartFile> file);
+	List<String> uploadImages(List<MultipartFile> files, String folderName);
 }
--- src/main/java/com/sikhye/chabak/utils/aws/S3UploadServiceImpl.java
+++ src/main/java/com/sikhye/chabak/utils/aws/S3UploadServiceImpl.java
@@ -4,6 +4,7 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.sikhye.chabak.base.exception.BaseException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,9 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+import static com.sikhye.chabak.base.BaseResponseStatus.S3_FORMAT_ERROR;
+import static com.sikhye.chabak.base.BaseResponseStatus.S3_UPLOAD_ERROR;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -24,8 +28,10 @@ public class S3UploadServiceImpl implements BasicUploadService {
 	private final S3Component component;
 
 	@Override
-	public String uploadImage(MultipartFile file) {
+	public String uploadImage(MultipartFile file, String folderName) {
 		String filename = createFilename(file.getOriginalFilename());
+		String pathWithName = folderName + filename;
+		log.info("S3_Upload_Filename = {}", pathWithName);
 
 		ObjectMetadata objectMetadata = new ObjectMetadata();
 		objectMetadata.setContentLength(file.getSize());
@@ -33,18 +39,20 @@ public class S3UploadServiceImpl implements BasicUploadService {
 
 		try {
 			InputStream inputStream = file.getInputStream();
-			uploadFile(inputStream, objectMetadata, filename);
+			uploadFile(inputStream, objectMetadata, pathWithName);
 		} catch (IOException e) {
-			throw new IllegalArgumentException("파일변환 중 에러 발생하였습니다.");
+			throw new BaseException(S3_UPLOAD_ERROR);
 		}
-		return getFileUrl(filename);
+		return getFileUrl(pathWithName);
 	}
 
 	@Override
-	public List<String> uploadImages(List<MultipartFile> file) {
-		return file.stream()
-			.map(this::uploadImage)
+	public List<String> uploadImages(List<MultipartFile> files, String folderName) {
+
+		return files.stream()
+			.map(file -> uploadImage(file, folderName))
 			.collect(Collectors.toList());
+
 	}
 
 
@@ -57,16 +65,17 @@ public class S3UploadServiceImpl implements BasicUploadService {
 		try {
 			return filename.substring(filename.lastIndexOf("."));
 		} catch (StringIndexOutOfBoundsException e) {
-			throw new IllegalArgumentException(String.format("잘못된 형식의 파일입니다."));
+			throw new BaseException(S3_FORMAT_ERROR);
 		}
+
 	}
 
-	private void uploadFile(InputStream inputStream, ObjectMetadata objectMetadata, String filename) {
-		amazonS3.putObject(new PutObjectRequest(component.getBucket(), filename, inputStream, objectMetadata)
+	private void uploadFile(InputStream inputStream, ObjectMetadata objectMetadata, String pathWithName) {
+		amazonS3.putObject(new PutObjectRequest(component.getBucket(), pathWithName, inputStream, objectMetadata)
 			.withCannedAcl(CannedAccessControlList.PublicRead));
 	}
 
-	private String getFileUrl(String filename) {
-		return amazonS3.getUrl(component.getBucket(), filename).toString();
+	private String getFileUrl(String pathWithName) {
+		return amazonS3.getUrl(component.getBucket(), pathWithName).toString();
 	}
 }

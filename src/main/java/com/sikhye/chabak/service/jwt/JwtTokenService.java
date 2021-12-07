20409ca259af556abe1e47fc0ec83832dce84507
hjun-park <phj0860@gmail.com>
2021-12-07 16:21:12 +0900

    Chore: Interceptor 계층 추가, JWT 토큰 서비스 변경

--- src/main/java/com/sikhye/chabak/service/jwt/JwtTokenService.java
+++ src/main/java/com/sikhye/chabak/service/jwt/JwtTokenService.java
@@ -1,16 +1,17 @@
 package com.sikhye.chabak.service.jwt;
 
 import static com.sikhye.chabak.global.response.BaseResponseStatus.*;
+import static com.sikhye.chabak.service.jwt.constant.JwtValue.*;
 
 import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import com.sikhye.chabak.global.config.ConfigProperties;
 import com.sikhye.chabak.global.exception.BaseException;
 import com.sikhye.chabak.service.member.constant.BaseRole;
 
@@ -19,44 +20,46 @@ import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
-@Service
+@Component
 public class JwtTokenService {
 
-	@Value("${secret.JWT_SECRET_KEY}")
-	private String JWT_SECRET_KEY;
+	private final ConfigProperties properties;
 
-	/*
-	JWT 생성
-	@param userIdx
-	@return String
-	 */
+	public JwtTokenService(ConfigProperties properties) {
+		this.properties = properties;
+	}
+
+	// 토큰 유효기간 4일
+	private final long tokenValidTime = 1000L * 60 * 60 * 24 * 4;
+
+	// JWT 토큰 생성
 	public String createJwt(Long memberId, BaseRole role) {
 		Date now = new Date();
 		return Jwts.builder()
 			.setHeaderParam("type", "jwt")
 			.claim("memberId", memberId)
-			.claim("role", role.toString())
-			.setIssuedAt(now)
-			.setExpiration(new Date(System.currentTimeMillis() + 1 * (1000 * 60 * 60 * 24 * 365)))    // TODO: 토큰유효 3일
-			.signWith(SignatureAlgorithm.HS256, JWT_SECRET_KEY)
+			.claim("role", role.name())
+			.setIssuedAt(now) // 토큰 발행 시간 정보
+			.setExpiration(new Date(System.currentTimeMillis() + tokenValidTime)) // set Expire Time
+			.signWith(SignatureAlgorithm.HS256, properties.getJwtSecret())
 			.compact();
 	}
 
 	/*
-	Header에서 X-ACCESS-TOKEN 으로 JWT 추출
-	@return String
-	 */
+		Header에서 X-ACCESS-TOKEN 으로 JWT 추출
+		@return String
+ 	*/
 	public String getJwt() {
 		HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes()).getRequest();
-		return request.getHeader("X-ACCESS-TOKEN");
+		return request.getHeader(X_ACCESS_TOKEN.toString());
 	}
 
 	/*
-	JWT에서 유저권한 추출
-	@return String
-	@throws BaseException
-	 */
-	public String getMemberRole() throws BaseException {
+		JWT에서 유저권한 추출
+		@return String
+		@throws BaseException
+ 	*/
+	public BaseRole getMemberRole() throws BaseException {
 		//1. JWT 추출
 		String accessToken = getJwt();
 		if (accessToken == null || accessToken.length() == 0) {
@@ -67,7 +70,7 @@ public class JwtTokenService {
 		Jws<Claims> claims;
 		try {
 			claims = Jwts.parser()
-				.setSigningKey(JWT_SECRET_KEY)
+				.setSigningKey(properties.getJwtSecret())
 				.parseClaimsJws(accessToken);
 		} catch (Exception ignored) {
 			throw new BaseException(INVALID_JWT);
@@ -75,14 +78,14 @@ public class JwtTokenService {
 
 		// 3. userIdx 추출
 		// ptpt: Object To Long
-		return String.valueOf(claims.getBody().get("role"));
+		return BaseRole.valueOf(String.valueOf(claims.getBody().get("role")));
 	}
 
 	/*
-	JWT에서 userIdx 추출
-	@return int
-	@throws BaseException
-	 */
+		JWT에서 userIdx 추출
+		@return int
+		@throws BaseException
+ 	*/
 	public Long getMemberId() throws BaseException {
 		//1. JWT 추출
 		String accessToken = getJwt();
@@ -94,7 +97,7 @@ public class JwtTokenService {
 		Jws<Claims> claims;
 		try {
 			claims = Jwts.parser()
-				.setSigningKey(JWT_SECRET_KEY)
+				.setSigningKey(properties.getJwtSecret())
 				.parseClaimsJws(accessToken);
 		} catch (Exception ignored) {
 			throw new BaseException(INVALID_JWT);
@@ -105,24 +108,15 @@ public class JwtTokenService {
 		return Long.valueOf(String.valueOf(claims.getBody().get("memberId")));
 	}
 
-	public Long getUserEmail(String token) throws BaseException {
-		//1. JWT 추출
-		if (token == null || token.length() == 0) {
-			throw new BaseException(EMPTY_JWT);
-		}
-
-		// 2. JWT parsing
-		Jws<Claims> claims;
+	// 토큰의 유효성 + 만료일자 확인
+	public boolean validateToken(String jwtToken) {
 		try {
-			claims = Jwts.parser()
-				.setSigningKey(JWT_SECRET_KEY)
-				.parseClaimsJws(token);
-		} catch (Exception ignored) {
-			throw new BaseException(INVALID_JWT);
+			Jws<Claims> claims = Jwts.parser()
+				.setSigningKey(properties.getUserInfoPasswordKey())
+				.parseClaimsJws(jwtToken);
+			return !claims.getBody().getExpiration().before(new Date());
+		} catch (Exception e) {
+			return false;
 		}
-
-		// 3. userIdx 추출
-		return Long.valueOf(String.valueOf(claims.getBody().get("memberId")));
 	}
-
 }

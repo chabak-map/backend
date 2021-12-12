5af426ccbeddc38522de567ecbc72de66ab6af50
hjun-park <phj0860@gmail.com>
2021-12-12 13:24:47 +0900

    fix: `@ConfigurationProperties` setter -> 생성자 주입 방식으로 변경

--- src/main/java/com/sikhye/chabak/ChabakApplication.java
+++ src/main/java/com/sikhye/chabak/ChabakApplication.java
@@ -2,12 +2,16 @@ package com.sikhye.chabak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import com.sikhye.chabak.global.config.ConfigProperties;
 
 @EnableCaching
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableConfigurationProperties(value = {ConfigProperties.class})
 public class ChabakApplication {
 
 	public static void main(String[] args) {
